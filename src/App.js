f757a65db01d4dc1a7e2ab1f0a17e5c995b3ed4e
Dmytro Byndin <129187393+DimaWebFrontEnd@users.noreply.github.com>
2025-04-29 13:37:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Check from "./img/chekIcon.png";
 
 function App() {
 
-  /* Те що вкладається через useState може потомім бити замінено даними з сервера */
   const [productName, setProductName] = useState("Мереживна накидка");
   const [imgProduct, setImgProduct] = useState(img);
   const [checkList, setCheckList] = useState(Check);
